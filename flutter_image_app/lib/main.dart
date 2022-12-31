c6bd9a4f4869e8a808e8908fe5f0164f0e1b229f
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-31 14:26:31 +0500

    12-31-2022

---
+++ flutter_image_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_image_app/Screens/HomeScreen.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: HomeScreen(),
+      );
+  }
+}
